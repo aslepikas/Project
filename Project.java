54842f41807d00e7cdf6a6d41974adaa0f2fff4c
aslepikas <slepikas@gmail.com>
2014-10-01 12:22:46 +0100

    changed location and made a mino change

--- Project.java
+++ Project.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
-
-//import java.awt.*;
+import src.Container;
+import java.awt.*;
 
 public class Project {
 	
---
+++ src/drawing/MyPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+
+    public JPanel MyPanel () {
+        new JPanel();
+    }
+
+}
