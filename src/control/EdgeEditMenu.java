5ec41a5b0af92fbc48ccdb10d94b5ad898cbfbac
aslepikas <slepikas@gmail.com>
2014-11-17 17:46:44 +0000

    almost finished with the message popup

--- src/control/EdgeEditMenu.java
+++ src/control/EdgeEditMenu.java
@@ -18,6 +18,8 @@ import javax.swing.JTextField;
 
 import model.Edge;
 
+import org.apache.commons.lang3.*;
+
 @SuppressWarnings("serial")
 public class EdgeEditMenu extends JDialog implements ActionListener,
 		PropertyChangeListener {
@@ -44,7 +46,7 @@ public class EdgeEditMenu extends JDialog implements ActionListener,
 			textFields.add(new JTextField(i));
 		}
 		textFields.add(new JTextField());
-		
+
 		String msg = "Enter transition labels:";
 
 		changePanel = new JPanel();
@@ -54,7 +56,7 @@ public class EdgeEditMenu extends JDialog implements ActionListener,
 		labelPanel.setLayout(labelPanelLayout);
 		for (int i = 0; i < textFields.size(); i++)
 			labelPanel.add(textFields.get(i));
-		
+
 		changePanel.setLayout(new BorderLayout());
 
 		moreButton = new JButton("more");
@@ -68,9 +70,10 @@ public class EdgeEditMenu extends JDialog implements ActionListener,
 				JOptionPane.OK_CANCEL_OPTION, null, options, options[0]);
 		setContentPane(optionPane);
 		// setDefaultCloseOperation();
-		this.setMinimumSize(new Dimension(300, textFields.size()*19 + 100)); // 19*#labels +
-														// buttonheight +
-														// buttonheight + c
+		this.setMinimumSize(new Dimension(300, textFields.size() * 19 + 100)); // 19*#labels
+																				// +
+		// buttonheight +
+		// buttonheight + c
 		System.out.println(getHeight());
 		System.out.println(getWidth());
 
@@ -105,31 +108,52 @@ public class EdgeEditMenu extends JDialog implements ActionListener,
 	@Override
 	public void propertyChange(PropertyChangeEvent e) {
 
+		boolean finish = true;
 		String val = e.getPropertyName();
-
+		System.out.println("tracker1");
 		if ((e.getSource() == optionPane)
 				&& (JOptionPane.VALUE_PROPERTY.equals(val) || JOptionPane.INPUT_VALUE_PROPERTY
 						.equals(val))) {
 
 			Object value = optionPane.getValue();
-
+			if (value == JOptionPane.UNINITIALIZED_VALUE) {
+				// ignore reset
+				return;
+			}
+			
+			optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
 			if (buttonstr1.equals(value)) {
+				System.out.println("stuff");
 				ArrayList<String> retVals = new ArrayList<String>();
 				for (JTextField i : textFields) {
-					retVals.add(i.getText());
-					System.out.println(i.getText());
+					if (i.getText().compareTo("") != 0) {
+						if (StringUtils.isAlphanumeric(i.getText())) {
+							retVals.add(i.getText());
+							System.out.println(i.getText()); // TODO : delete
+																// the
+																// comment
+							finish = finish && true;
+						} else {
+							JOptionPane.showMessageDialog(this,
+									"Alphanumeric characters only");
+							System.out.println("tracker");
+							finish = false;
+							break;
 
-					// TODO : add string confirmation
+						}
+						// TODO : add string confirmation
+					}
 				}
 				edge.setLabels(retVals);
 			} else {
-				//currently nothing is happening after pressing cancel button
+				// currently nothing is happening after pressing cancel button
 			}
-			// TODO make the below method work
-			this.getRootPane().repaint();
-			this.dispose();
+			if (finish) {
+				this.getRootPane().repaint();
+				this.dispose();
+			}
 		}
-
+		// TODO make the below method work
 	}
 
 	@Override
